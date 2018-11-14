596908aa573a0705999dcdf8e77ccc8363bf821b
olijng <olijng7@gmail.com>
2018-11-13 21:49:26 -0500

    Modified Tour and TourList to skeleton code format

--- src/Tour.java
+++ src/Tour.java
@@ -1,2 +1,97 @@
+import java.io.File;
+import java.util.Date;
+
 public class Tour {
+
+    private File infoFile;
+    private String name;
+    private double cost;
+    private String description;
+    private String location;
+    private String itinerary;
+    private Date date;
+    private int idNumber;
+    private boolean isCancelled;
+
+    /**
+     * Constructs a tour object by reading a formatted tour text file
+     * @param infoFile formatted text file
+     */
+    public Tour(File infoFile) {
+        this.infoFile = infoFile;
+    }
+
+    //getters and setters. Only admins will be able to set these values.
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name, Account accnt) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost, Account accnt) {
+        this.cost = cost;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description, Account accnt) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location, Account accnt) {
+        this.location = location;
+    }
+
+    public String getItinerary() {
+        return itinerary;
+    }
+
+    public void setItinerary(String itinerary, Account accnt) {
+        this.itinerary = itinerary;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date, Account accnt) {
+        this.date = date;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber, Account accnt) {
+        this.idNumber = idNumber;
+    }
+
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    public void setCancelled(boolean cancelled, Account accnt) {
+        isCancelled = cancelled;
+    }
+
+    /**
+     * Returns a readable, formatted string tour.
+     * @return tour string
+     */
+    @Override
+    public String toString(){
+        return null;
+    }
 }
--- src/TourList.java
+++ src/TourList.java
@@ -1,2 +1,52 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public class TourList {
+
+    private File toursFile;
+    private ArrayList<Integer> tourIDlist;
+    private ArrayList<Tour> tourList;
+
+    /**
+     * Creates a new TourList given a file of Tour ID's.
+     * Reads ID's from file then finds each corresponding tour file in tours directory and constructs new tour from it.
+     * Adds tour to tourList. Adds ID to tourIDlist (might want to make that a hashmap?).
+     * @param toursFile file of tour IDs
+     */
+    public TourList(File toursFile) {
+        this.toursFile = toursFile;
+    }
+
+    /**
+     * Gets a tour using it's ID number.
+     * @param id id number of tour
+     * @return tour with that ID number
+     */
+    public Tour selectTour(int id){
+        return null;
+    }
+
+    /**
+     * Generates a list of tours in a certain order determined by the index.
+     * 0 = id number (list all)
+     * 1 = name
+     * 2 = date
+     * 3 = location
+     * 4 = cost
+     * 5 = status (cancelled or not)
+     * @param index type of search
+     * @return list of tours
+     */
+    public ArrayList list(int index){
+        return tourIDlist;
+    }
+
+    /**
+     * returns the list of tours as a readable string
+     * @return readable list
+     */
+    @Override
+    public String toString(){
+        return null;
+    }
 }
